a2bafc5a4c01dc7d3e03a1160407819669d98045
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-16 13:06:01 +0100

    test script 1

---
+++ arch-installer.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "Launch of automatic installation of arch script..."
+
+$system=""
+
+loadkeys fr
+if [[ $(ls /sys/firmware/efi/efivars)]]; then
+    echo "efi systeme"
+    $system="efi"
+else
+    echo "BIOS systeme"
+    $system="bios"
+fi
+
+echo "pingging archlinux.org ...."
+if [ "`ping -c 1 archlinux.org`" ]
+then
+  echo "ping success"
+else
+  echo "ping failed.."
+  exit 1
+fi
+
+timedatectl set-ntp true
+
+diskchoice()
+{
+$i=1
+for diskname in $(lsblk -o NAME)
+do
+if [ $i = $1 ]
+${DISK} = $diskname
+then 
+$i=$i+1
+done
+}
+
+$i=1
+for diskname in $(lsblk -o NAME)
+do
+echo "$i"
+echo "$diskname"
+$i=$i+1
+done
+
+echo -e "choose on which disk you want to install archlinux : "
+
+read choice
+
+case $choice in
+  1) $disk = diskchoice "1" 
+  2) $disk = diskchoice "2"
+  3) $disk = diskchoice "3"
+  *) echo "ERROR"
+esac
+
+echo "you choose $disk"
