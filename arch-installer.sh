586d61ab316bc093f9a59b49fbfb022c663d475b
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-16 20:40:45 +0100

    a

--- arch-installer.sh
+++ arch-installer.sh
@@ -22,9 +22,10 @@ fi
 
 timedatectl set-ntp true
 
+#!--------------------------------------Disk choice----------------------------------------!#
 diskchoice()
 {
-i=0
+i=-1
 for diskname in $(lsblk -o NAME)
 do  
     if [ $i = $1 ];then
@@ -38,17 +39,24 @@ i=0
 for diskname in $(lsblk -o NAME)
 do
   if [ $i != 0 ];then 
-        echo "$i) $diskname"
+        #echo "$i) $diskname"
+        disk$i = $diskname
   fi
 
 i=$((i + 1))
 done
 
-echo -e "choose on which disk you want to install archlinux : "
+dialog --menu "Choose one:" 10 30 3 1 $disk1 2 $disk2\ 3 $disk3 \ 4 $disk4 \
+--title "Disk choice" \
+--msgbox 'choose on which disk you want to install archlinux :' \
+--checklist 
 
-read choice
 
-case $choice in
+# echo -e "choose on which disk you want to install archlinux : "
+
+read choiceD
+
+case $choiceD in
   "1") diskchoice "0" ;;
   "2") diskchoice "1";;
   "3") diskchoice "2";;
@@ -58,3 +66,20 @@ case $choice in
 esac
 
 echo "you choose the disk ${DISK}"
+
+
+#!--------------------------------------Partition----------------------------------------!#
+
+if [ $system = efi ];then 
+  echo -e "Do you want a SWAP partition [y/n]"
+  
+  read choiceS
+ 
+  if [ choiceS = "y" || choiceS = "Y" ], then
+        echo -e "Enter the size of the partition "
+  
+  else
+  
+  
+  fi
+fi
