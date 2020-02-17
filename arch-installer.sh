9d5e0b5d0d6d58baf987c0366417d583b7b4cc49
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-18 00:01:23 +0100

    a

--- arch-installer.sh
+++ arch-installer.sh
@@ -84,9 +84,7 @@ dialog --title "SWAP"  --yesno "Do you want a SWAP partition ?" 6 20 3>&1 1>&2 2
 swap=$?
 echo "$swap"
 
-  dialog --title "Inputbox - To take input from you" \
-      --backtitle "Linux Shell Script Tutorial Example" \
-      --inputbox "Enter your name " 8 60 2)
+  user_input=$(dialog --title "Create Directory" --inputbox "Enter the directory name:" 8 40)
       echo "$swapsize"
       sleep 5
 exit
