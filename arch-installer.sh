7b8a90508c609fef71f3b9a5ae6fb8590cc10bf6
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-17 11:31:40 +0100

    a

--- arch-installer.sh
+++ arch-installer.sh
@@ -48,10 +48,12 @@ done
  
 
 dialog --title "A dialog Menu Example" \
---menu "Please choose an option:" 15 55 5 \
-1 "Add a record to DB" \
-2 "Delete a record from DB" \
-3 "Exit from this menu"
+--menu "choose on which disk you want to install archlinux : " 20 60 10 \
+1 disk[1] \
+2 disk[2] \
+3 disk[3] \
+4 disk[4] \
+5 disk[5] \
 
 
 
