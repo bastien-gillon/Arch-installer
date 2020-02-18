cdacca9cff0cfe9729cccd74e5d6bb3dbea9659b
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-18 12:29:46 +0100

    a

--- arch-installer.sh
+++ arch-installer.sh
@@ -84,13 +84,9 @@ dialog --title "SWAP"  --yesno "Do you want a SWAP partition ?" 6 20 3>&1 1>&2 2
 swap=$?
 echo "$swap"
 
-dialog --title "Inputbox - To take input from you" \
---backtitle "Linux Shell Script Tutorial Example" \
---inputbox "Enter your name " 8 60 2
-
 
 case $swap in
-   0)  dialog --title "Swap Size" \      
+   0)  dialog --title "Swap Size" \
       --inputbox "Enter a size for the swap partition (ex: 512M or 1G)" 8 60 2
       swapsize=$?
       echo "$swapsize" ;;
