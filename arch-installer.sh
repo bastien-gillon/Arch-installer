ff99e8b7f9485ad8739814ea52f356b2b186ef89
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-18 15:13:51 +0100

    a

--- arch-installer.sh
+++ arch-installer.sh
@@ -94,8 +94,8 @@ swap=$?
 
 case $swap in
    0)  swapsize=$(dialog --title "Swap Size" \
-      --inputbox "Enter a size for the swap partition (ex: 512M or 1G)" \
-      --backtitle "Size of the disk : $DISKSIZE"  8 60 3>&1 1>&2 2>&3 3>&- ) ;;
+      --inputbox "Enter a size for the swap partition (ex: 512M or 1G)" 8 60 \
+      --backtitle "Size of the disk : $DISKSIZE" 3>&1 1>&2 2>&3 3>&- ) ;;
      
    1) swapsize="no" ;;
    255) exit ;;
@@ -106,7 +106,7 @@ rootpartition=$?
 
 case $rootpartition in
    0) sizerootpartition=$(dialog --title "/ Size" \
-      --inputbox "Enter a size for the / partition (ex: 512M or 1G)"  --backtitle "Size of the disk \: ${DISKSIZE} , size of the swap: $swapsize" 8 60 3>&1 1>&2 2>&3 3>&- ) ;;
+      --inputbox "Enter a size for the / partition (ex: 512M or 1G)" 8 60 --backtitle "Size of the disk \: ${DISKSIZE} , size of the swap: $swapsize" 3>&1 1>&2 2>&3 3>&- ) ;;
    1) sizerootpartition="no" ;;
    255) exit 1;;
 esac
@@ -116,7 +116,7 @@ homepartition=$?
 
 case $homepartition in
    0) sizehomepartition=$(dialog --title "/home Size" \
-      --inputbox "Enter a size for the /home partition (ex: 512M or 1G)" --backtitle "Size of the disk \: ${DISKSIZE} , size of the swap: $swapsize, size of the / partition $sizerootpartition"  8 60 3>&1 1>&2 2>&3 3>&- ) ;;
+      --inputbox "Enter a size for the /home partition (ex: 512M or 1G)"  8 60 --backtitle "Size of the disk \: ${DISKSIZE} , size of the swap: $swapsize, size of the / partition $sizerootpartition"  3>&1 1>&2 2>&3 3>&- ) ;;
    1) sizehomepartition="no" ;;
    255) exit 1;;
 esac
