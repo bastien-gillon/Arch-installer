b7549fa639049c41a0bc7a0a60ab69801b055568
bastien-gillon <bastien.gillon@protonmail.com>
2020-02-27 16:28:57 +0100

    a

--- arch-installer.sh
+++ arch-installer.sh
@@ -76,7 +76,7 @@ done
 
 DISKSIZE=${TABSIZE[$DISKID+1]} 
 
-dialog --title "SWAP"  --yesno "Do you want a SWAP partition ?" 10 60 3>&1 1>&2 2>&3 3>&- 
+dialog --title "SWAP"  --yesno "Do you want a SWAP partition ?" 10 60 3>&1 1>&2 2>&3 3>&- 741
 swap=$?
 
 
