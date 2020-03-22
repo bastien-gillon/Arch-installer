9473543ecd8c3f7603541f181a8afb3bf797203d
bastien-gillon <bastien.gillon@protonmail.com>
2020-03-22 11:56:27 +0100

    a

--- chroot.sh
+++ chroot.sh
@@ -1,19 +1,19 @@
-#COUNT=0
-#zoneinfo=""
-#for i in $(ls /usr/share/zoneinfo/)
-#do
-#  zoneinfo="$zoneinfo $i"
-#  COUNT=$[COUNT+1]
-#done
+COUNT=0
+zoneinfo=""
+cd usr/share/zoneinfo
+for i in $(ls -d -- */ )
+do
+COUNT=$[COUNT+1]
+  zoneinfo="$zoneinfo $COUNT $i"
+done
 
-ZONE=$(dialog --title "List file of directory" --fselect /usr/share/zoneinfo/ 100 100 3>&1 1>&2 2>&3 3>&- )
-zone1=$?
-#ZONE=$(\
-#dialog --title "Disk Choice"\
-# --menu "choose your zone : "  20 70 10 \
-# $zoneinfo 3>&1 1>&2 2>&3 3>&- )
+
+ZONE=$(\
+dialog --title "Disk Choice"\
+ --menu "choose your zone : "  20 70 10 \
+ $zoneinfo 3>&1 1>&2 2>&3 3>&- )
 
 
 echo "$ZONE"
-echo "$zone1"
+
 exit
