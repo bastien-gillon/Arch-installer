c43d4b687d372aafe9713f18862b913e38eb877d
bastien-gillon <bastien.gillon@protonmail.com>
2020-03-23 19:35:03 +0100

    a

--- chroot.sh
+++ chroot.sh
@@ -1,5 +1,7 @@
 COUNT=0
 zoneinfo=""
+
+##-----------CHOICE OF ZONE-----------#
 cd /usr/share/zoneinfo
 tmp=$(ls -d -- */)
 echo $tmp
@@ -16,7 +18,7 @@ dialog --title "Disk Choice"\
  $zoneinfo 3>&1 1>&2 2>&3 3>&- )
 
 
-
+##-----------CHOICE OF REGION-----------#
 cd /usr/share/zoneinfo/TABZONE[$ZONE]
 region=""
 echo $tmp
@@ -24,10 +26,15 @@ for i in $(ls)
 do
 COUNT=$[COUNT+1]
   region="$region $COUNT $i"
-  TABZONE[$COUNT]=$i
+  TABREGION[$COUNT]=$i
 done
+REGION=$(\
+dialog --title "Disk Choice"\
+ --menu "choose your zone : "  20 70 10 \
+ $region 3>&1 1>&2 2>&3 3>&- )
+
 
 echo TABZONE[$ZONE]
-echo TABZONE[$region]
+echo TABREGION[$REGION]
 
 exit
