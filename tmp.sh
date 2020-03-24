4a1aa5b3b44a3aad0569492e3e928599aa464308
bastien-gillon <bastien.gillon@protonmail.com>
2020-03-24 14:06:26 +0100

    a

--- tmp.sh
+++ tmp.sh
@@ -30,8 +30,7 @@ echo "sleep 3"
 umount -R /mnt
 
 echo " before "
-
-dialog 
+whiptail --title "I am the title" --msgbox "Hi there" 8 45
 dialog --title "Reboot"  --msgbox 'Your installation is finished. Your pc will reboot ...' 7 30 3>&1 1>&2 2>&3 3>&-
 
 echo "================="
